ea15035bb4265d99480b922d0188946be45744be
vpalex999 <vpalex@mail.ru>
2022-05-23 11:28:47 +0500

    6.0 Labeling the field

--- appointments/src/CustomerForm.js
+++ appointments/src/CustomerForm.js
@@ -1,4 +1,13 @@
 import React from 'react';
 
 
-export const CustomerForm = () => <form id="customer" />;
+export const CustomerForm = ({ firstName }) => <form id="customer" >
+  <label htmlFor='firstName'>First name</label>
+  <input
+    type='text'
+    name='firstName'
+    id='firstName'
+    value={firstName}
+    readOnly
+  />
+</form>;
