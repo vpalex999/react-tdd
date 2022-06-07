227c02f7e7f620ffaa35729ce88929e1cee53efd
vpalex999 <vpalex@mail.ru>
2022-06-07 19:59:43 +0500

    8.2 Completting the tests for the select box

--- appointments/src/AppointmentForm.js
+++ appointments/src/AppointmentForm.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export const AppointmentForm = () => (
-  <form id='appointment' >
-    <select name='service' />
-  </form>
-);
+export const AppointmentForm = ({ selectableServices, service, onSubmit }) => {
+
+  const [appointment, setAppointment] = useState({ service });
+
+  const handleServiceChange = ({ target: { value } }) =>
+    setAppointment((appointment) = ({
+      ...appointment,
+      service: value,
+    }));
+
+
+  return (
+    <form id='appointment' onSubmit={() => onSubmit(appointment)}>
+      <label htmlFor="service">Salon service</label>
+      <select
+        id="service"
+        name='service'
+        value={service}
+        onChange={handleServiceChange}
+      >
+        <option />
+        {selectableServices.map((s) => (
+          <option key={s}>{s}</option>
+        ))}
+      </select>
+    </form>
+  );
+};
+
+AppointmentForm.defaultProps = {
+  selectableServices: [
+    'Cut',
+    'Blow-dry',
+    'Cut & color',
+    'Beard trim',
+    'Cut & bread trim',
+    'Extensions',
+  ]
+};
--- appointments/test/AppointmentForm.test.js
+++ appointments/test/AppointmentForm.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactTestUtils from 'react-dom/test-utils';
 import { createContainer } from './domManipulators';
 import { AppointmentForm } from '../src/AppointmentForm';
 
@@ -20,11 +21,74 @@ describe('AppointmentForm', () => {
   describe('service field', () => {
     const field = (name) => form('appointment').elements[name];
 
+    const findOption = (dropdownNode, textContent) => {
+      const options = Array.from(dropdownNode.childNodes);
+      return options.find((option) => option.textContent === textContent);
+    };
+
     it('renders as a select box', () => {
       render(<AppointmentForm />);
 
       expect(field('service')).not.toBeNull();
       expect(field('service').tagName).toEqual('SELECT');
+    });
+
+    it('initially has a blank value chosen', () => {
+      render(<AppointmentForm />);
+      const firstNode = field('service').childNodes[0];
+
+      expect(firstNode.value).toEqual('');
+      expect(firstNode.selected).toBeTruthy();
+    });
+
+    it('lists all salon services', () => {
+      const selectableServices = ['Cut', 'Blow-dry'];
+
+      render(<AppointmentForm selectableServices={selectableServices} />);
+
+      const optionNodes = Array.from(field('service').childNodes);
+      const renderedServices = optionNodes.map((node) => node.textContent);
+
+      expect(renderedServices).toEqual(
+        expect.arrayContaining(selectableServices)
+      );
+    });
+
+    it('pre-selects the existing value', () => {
+      const services = ['Cut', 'Blow-dry'];
+      render(<AppointmentForm selectableServices={services} service='Blow-dry' />);
+
+      const option = findOption(field('service'), 'Blow-dry');
+
+      expect(option.selected).toBeTruthy();
+    });
+
+    it('save existing value when submitted', async () => {
+      expect.hasAssertions();
+
+      render(
+        <AppointmentForm
+          service='Blow-dry'
+          onSubmit={({ service }) => expect(service).toEqual('Blow-dry')} />
+      );
+
+      ReactTestUtils.Simulate.submit(form('appointment'));
+    });
+
+    it.skip('save new value when submitted', async () => {
+      expect.hasAssertions();
+
+      render(
+        <AppointmentForm
+          service='Blow-dry'
+          onSubmit={({ service }) => expect(service).toEqual('Cut')} />
+      );
+
+      ReactTestUtils.Simulate.change(field('service'), {
+        target: { value: 'Cut', name: 'service' },
+      });
+
+      ReactTestUtils.Simulate.submit(form('appointment'));
     });
   });
 });
